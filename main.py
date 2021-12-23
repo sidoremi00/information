c6d850ce54ede4753c136c89333dd8748a85ab28
sidoremi00 <92983634+sidoremi00@users.noreply.github.com>
2021-12-23 12:40:05 +0900

    pxt.json, main.blocks, main.ts, main.py 업데이트하기

--- main.py
+++ main.py
@@ -1,46 +1,83 @@
-def score (Score: number, Winner: str):
+def on_button_pressed_a():
+    global Re
+    Re = 1
+    basic.show_leds("""
+        # . . . #
+                . # . # .
+                . . # . .
+                . # . # .
+                # . . . #
+    """)
+    basic.show_leds("""
+        # . . . #
+                . # . # .
+                . . . . .
+                . # . # .
+                # . . . #
+    """)
+    basic.show_leds("""
+        # . . . #
+                . . . . .
+                . . . . .
+                . . . . .
+                # . . . #
+    """)
+    basic.pause(100)
+    control.reset()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def score(Score: number, Winner: str):
     global 석진이
-    if Score >= 5:
-        석진이 = "Finish" 
-        basic.show_string("Game over")
+    if Score >= 1:
+        basic.show_string("GG")
         basic.show_string("Winner is")
         basic.show_string(Winner)
+        basic.pause(100)
+        석진이 = "Finish"
     else:
         석진이 = "Non"
         basic.clear_screen()
-
-
+Re = 0
+석진이 = ""
 # 시작
-
-석진이 = "Non" 
+석진이 = "Non"
 Player_1 = 0
 Player_2 = 0
-
+Re = 0
 # 석진이
-# Non 
-# Play 
-# Finish 
-
+# Non
+# Play
+# Finish
 # 점이 보이고 먼저 클릭하면 1점 추가
-# 계속 반복 
+# 계속 반복
 
 def on_forever():
     global 석진이, Player_1, Player_2
-    if 석진이 == "Non":
-        basic.pause(randint(1000, 5000)) # 랜덤으로 시간 설저
-        led.plot(randint(0, 4), randint(0, 4)) # 랜덤 좌표
-        석진이 = "Play" 
-    if 석진이 == "Play":
-        if input.pin_is_pressed(TouchPin.P1): # 1에 신호가 감지 되었을때(player a가 버튼을 눌렀을 때)
-            score(Player_1, "A") 
-            Player_1 += 1
-            basic.show_arrow(ArrowNames.WEST)
-            basic.clear_screen()
-            basic.pause(10)
-        if input.pin_is_pressed(TouchPin.P2): # 2에 신호가 감지 되었을때(player b가 버튼을 눌렀을 때)
-            score(Player_2, "B")
-            Player_2 += 1
-            basic.show_arrow(ArrowNames.EAST)
-            basic.clear_screen()
-            basic.pause(10)
+    while Re == 0:
+        if 석진이 == "Non":
+            basic.pause(randint(1000, 5000))
+            # 랜덤으로 시간 설저
+            led.plot(randint(0, 4), randint(0, 4))
+            # 랜덤 좌표
+            석진이 = "Play"
+        if 석진이 == "Play":
+            if input.pin_is_pressed(TouchPin.P1):
+                # 1에 신호가 감지 되었을때(player a가 버튼을 눌렀을 때)
+                score(Player_1, "A")
+                Player_1 += 1
+                basic.show_arrow(ArrowNames.WEST)
+                basic.clear_screen()
+                basic.pause(10)
+            if input.pin_is_pressed(TouchPin.P2):
+                # 2에 신호가 감지 되었을때(player b가 버튼을 눌렀을 때)
+                score(Player_2, "B")
+                Player_2 += 1
+                basic.show_arrow(ArrowNames.EAST)
+                basic.clear_screen()
+                basic.pause(10)
+        if 석진이 == "Finish":
+            basic.show_string("Try again=A else=B")
+            if input.button_is_pressed(Button.B):
+                basic.show_icon(IconNames.SKULL)
+                break
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,31 +1,33 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
     Re = 1
     basic.showLeds(`
         # . . . #
-        . # . # .
-        . . # . .
-        . # . # .
-        # . . . #
-        `)
+                . # . # .
+                . . # . .
+                . # . # .
+                # . . . #
+    `)
     basic.showLeds(`
         # . . . #
-        . # . # .
-        . . . . .
-        . # . # .
-        # . . . #
-        `)
+                . # . # .
+                . . . . .
+                . # . # .
+                # . . . #
+    `)
     basic.showLeds(`
         # . . . #
-        . . . . .
-        . . . . .
-        . . . . .
-        # . . . #
-        `)
+                . . . . .
+                . . . . .
+                . . . . .
+                # . . . #
+    `)
     basic.pause(100)
     control.reset()
 })
-function score (Score: number, Winner: string) {
-    if (Score >= 5) {
+function score(Score: number, Winner: string) {
+    
+    if (Score >= 1) {
         basic.showString("GG")
         basic.showString("Winner is")
         basic.showString(Winner)
@@ -35,53 +37,62 @@ function score (Score: number, Winner: string) {
         석진이 = "Non"
         basic.clearScreen()
     }
+    
 }
+
 let Re = 0
 let 석진이 = ""
-// 시작
+//  시작
 석진이 = "Non"
 let Player_1 = 0
 let Player_2 = 0
 Re = 0
-// 석진이
-// Non
-// Play
-// Finish
-// 점이 보이고 먼저 클릭하면 1점 추가
-// 계속 반복
-basic.forever(function () {
+//  석진이
+//  Non
+//  Play
+//  Finish
+//  점이 보이고 먼저 클릭하면 1점 추가
+//  계속 반복
+basic.forever(function on_forever() {
+    
     while (Re == 0) {
         if (석진이 == "Non") {
             basic.pause(randint(1000, 5000))
-            // 랜덤으로 시간 설저
+            //  랜덤으로 시간 설저
             led.plot(randint(0, 4), randint(0, 4))
-            // 랜덤 좌표
+            //  랜덤 좌표
             석진이 = "Play"
         }
+        
         if (석진이 == "Play") {
             if (input.pinIsPressed(TouchPin.P1)) {
-                // 1에 신호가 감지 되었을때(player a가 버튼을 눌렀을 때)
+                //  1에 신호가 감지 되었을때(player a가 버튼을 눌렀을 때)
                 score(Player_1, "A")
                 Player_1 += 1
                 basic.showArrow(ArrowNames.West)
                 basic.clearScreen()
                 basic.pause(10)
             }
+            
             if (input.pinIsPressed(TouchPin.P2)) {
-                // 2에 신호가 감지 되었을때(player b가 버튼을 눌렀을 때)
+                //  2에 신호가 감지 되었을때(player b가 버튼을 눌렀을 때)
                 score(Player_2, "B")
                 Player_2 += 1
                 basic.showArrow(ArrowNames.East)
                 basic.clearScreen()
                 basic.pause(10)
             }
+            
         }
+        
         if (석진이 == "Finish") {
             basic.showString("Try again=A else=B")
             if (input.buttonIsPressed(Button.B)) {
                 basic.showIcon(IconNames.Skull)
-                break;
+                break
             }
+            
         }
+        
     }
 })
